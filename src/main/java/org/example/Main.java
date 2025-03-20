fb87b5c453f72794efd3ef69471297ca4aef3888
bhargav <bhargavva.r@lenskart.in>
2025-03-20 19:43:05 +0530

    Tic-Tac-Toe completedgit add .

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,15 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Tic-Tac-Toe");
+        Scanner scanner = new Scanner(System.in);
         Game game = new Game();
-        game.enterBoardDetails();
-        game.getPlayersDetails();
+        game.enterBoardDetails(scanner);
+        game.getPlayersDetails(scanner);
         game.beginGame();
+        scanner.close();
     }
 }
